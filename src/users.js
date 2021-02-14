954ef433cf27ff2e7d415892185de676617ab1fa
rabiaonal <56224122+rabiaonal@users.noreply.github.com>
2021-02-14 21:18:26 +0300

    Add files via upload

---
+++ src/users.js
@@ -0,0 +1,17 @@
+data = [ 
+{
+        Name:"Kenneth",
+        Email:"kenneth@gmail.com",
+        Password:329129293
+    },
+    {
+        Name:"Jhon",
+        Email:"john@gmail.com",
+        Password:329129293
+    },
+    {
+       Name:"MAry",
+        Email:"mary@gmail.com",
+        Password:329129293
+    }
+];
