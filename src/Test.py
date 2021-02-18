04a8ad9520c3b7a661211758a019bb17b0131b3e
rabional <nur.onal@ug.bilkent.edu.tr>
2021-02-18 22:44:47 +0300

    assert check

--- src/Test.py
+++ src/Test.py
@@ -15,23 +15,33 @@ invalid_passwords = ["123", "xyz", "...", "0123456789012345678901234567890123456
 unregistered_passwords = ["invalidpassword1", "invalidpassword2", "invalidpassword3"]
 registered_passwords = ["329129293", "1234", "123123123"]
 
+def assertCheck(test_descriptor, input, expected):
+    try:
+        assert input == expected
+        print(test_descriptor, "Test Succesful.")
+    except AssertionError:
+        print(test_descriptor, "Test Failed.", "Expected Result: ", expected)
+
 def index_page_sign_in_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signInBtn").click()
     sleep(1)
-    assert driver.current_url == "http://localhost:3000/login"
+    assertCheck("Index page sign in button test", driver.current_url, "http://localhost:3000/login")
+    #assert driver.current_url == "http://localhost:3000/login"
 
 def index_page_sign_up_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signUpBtn").click()
     sleep(1)
-    assert driver.current_url == "http://localhost:3000/signup"
+    assertCheck("Index page sign up button test", driver.current_url, "http://localhost:3000/signup")
+    #assert driver.current_url == "http://localhost:3000/signup"
 
 def login_page_home_button_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("homebutton").click()
-    sleep(2)
-    assert driver.current_url == "http://localhost:3000/"
+    sleep(1)
+    assertCheck("Login page home button test", driver.current_url, "http://localhost:3000/")
+    #assert driver.current_url == "http://localhost:3000/"
 
 def login_page_passhide_button_check():
     driver.get("http://localhost:3000/login")
@@ -41,27 +51,35 @@ def login_page_passhide_button_check():
     button_type_2 = button.get_attribute("type")
     button.click()
     button_type_3 = button.get_attribute("type")
-    assert button_type_1 != button_type_2 & button_type_1 == button_type_3
+    #assertCheck("Login page pass hide button check", driver.current_url, "http://localhost:3000/login")
+    try:
+        assert button_type_1 != button_type_2 & button_type_1 == button_type_3
+        print("Login page pass/hide button test: ", "Test Succesful.")
+    except AssertionError:
+        print("Login page pass/hide button test: ", "Test Failed.")
 
 
 def login_page_sign_up_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("signupLink").click()
     sleep(1)
-    assert driver.current_url == "http://localhost:3000/signup"
+    assertCheck("Login page sign up link test", driver.current_url, "http://localhost:3000/signup")
+    #assert driver.current_url == "http://localhost:3000/signup"
 
 
 def login_page_help_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("forgotpasswordLink").click()
     sleep(1)
-    assert driver.current_url == "http://localhost:3000/forgotpassword"
+    assertCheck("Login page help up link test", driver.current_url, "http://localhost:3000/forgotpassword")
+    #assert driver.current_url == "http://localhost:3000/forgotpassword"
 
 def signup_page_login_link_check():
     driver.get("http://localhost:3000/signup")
     driver.find_element_by_id("loginLink").click()
     sleep(1)
-    assert driver.current_url == "http://localhost:3000/login"
+    assertCheck("Sign up page login link test", driver.current_url, "http://localhost:3000/login")
+    #assert driver.current_url == "http://localhost:3000/login"
 
 
 # Check different input combinations
