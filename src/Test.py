59d4c61c5c12209fd8127171ab93cae200afb938
burak mutlu <burak_mutlu_97@hotmail.com>
2021-02-19 22:29:48 +0300

    compatibility test impl

--- src/Test.py
+++ src/Test.py
@@ -1,5 +1,6 @@
-from time import sleep
+import sys
 from selenium import webdriver
+from selenium.common.exceptions import SessionNotCreatedException, WebDriverException
 
 invalid_input = ""
 
@@ -18,24 +19,28 @@ registered_passwords = ["329129293", "1234", "123123123"] #Should be sorted with
 def assertCheck(test_descriptor, input, expected):
     try:
         assert input == expected
-        print(test_descriptor, "Test Successful.", "Expected Result:", expected)
+        if log_enabled:
+            print(test_descriptor, "Test Successful.", "Expected Result:", expected)
+        return True
     except AssertionError:
-        print(test_descriptor, "Test Failed.", "Expected Result:", expected, "Obtained Result:", input)
+        if log_enabled:
+            print(test_descriptor, "Test Failed.", "Expected Result:", expected, "Obtained Result:", input)
+        return False
 
 def index_page_sign_in_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signInBtn").click()
-    assertCheck("Index-page sign in button test:", driver.current_url, "http://localhost:3000/login")
+    return assertCheck("Index-page sign in button test:", driver.current_url, "http://localhost:3000/login")
 
 def index_page_sign_up_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signUpBtn").click()
-    assertCheck("Index-page sign up button test:", driver.current_url, "http://localhost:3000/signup")
+    return assertCheck("Index-page sign up button test:", driver.current_url, "http://localhost:3000/signup")
 
 def login_page_home_button_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("homebutton").click()
-    assertCheck("Login-page home button test:", driver.current_url, "http://localhost:3000/")
+    return assertCheck("Login-page home button test:", driver.current_url, "http://localhost:3000/")
 
 def login_page_passhide_button_check():
     driver.get("http://localhost:3000/login")
@@ -48,92 +53,246 @@ def login_page_passhide_button_check():
     input_type_3 = input.get_attribute("type")
     try:
         assert (input_type_1 != input_type_2) & (input_type_1 == input_type_3)
-        print("Login-page pass-hide button test:", "Test Successful.")
+        if log_enabled:
+            print("Login-page pass-hide button test:", "Test Successful.")
+        return True
     except AssertionError:
-        print("Login-page pass-hide button test:", "Test Failed.")
+        if log_enabled:
+            print("Login-page pass-hide button test:", "Test Failed.")
+        return False
 
 def login_page_sign_up_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("signupLink").click()
-    assertCheck("Login-page sign up link test:", driver.current_url, "http://localhost:3000/signup")
+    return assertCheck("Login-page sign up link test:", driver.current_url, "http://localhost:3000/signup")
 
 def login_page_help_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("forgotpasswordLink").click()
-    assertCheck("Login-page help link test:", driver.current_url, "http://localhost:3000/resetpass")
+    return assertCheck("Login-page help link test:", driver.current_url, "http://localhost:3000/resetpass")
 
 def signup_page_login_link_check():
     driver.get("http://localhost:3000/signup")
     driver.find_element_by_id("loginLink").click()
-    assertCheck("Signup-page sign in link test:", driver.current_url, "http://localhost:3000/login")
+    return assertCheck("Signup-page sign in link test:", driver.current_url, "http://localhost:3000/login")
 
 def input_check():
     driver.get(url_path + login_route)
-    print("Login with input: %s, password: %s" % (invalid_input, registered_passwords[0]))
+    if log_enabled:
+        print("Login with input: %s, password: %s" % (invalid_input, registered_passwords[0]))
     driver.find_element_by_id("email").send_keys(invalid_input)
     driver.find_element_by_id("pass").send_keys(registered_passwords[0])
     driver.find_element_by_id("loginSubmit").click()
-    assertCheck("Check invalid input test:", driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid e-mail or password.")
+    return assertCheck("Check invalid input test:", driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid e-mail or password.")
 
 def login_script(email, phone, password):
     driver.get(url_path + login_route)
     if email is None:
-        print("Login with phone: %s, password: %s" % (phone, password))
+        if log_enabled:
+            print("Login with phone: %s, password: %s" % (phone, password))
         driver.find_element_by_id("email").send_keys(phone)
     else:
-        print("Login with email: %s, password: %s" % (email, password))
+        if log_enabled:
+            print("Login with email: %s, password: %s" % (email, password))
         driver.find_element_by_id("email").send_keys(email)
     driver.find_element_by_id("pass").send_keys(password)
     driver.find_element_by_id("loginSubmit").click()
 
 def email_check():
+    results = { "invalid": [], "unregistered": [], "registered": [] }
     for i  in range(0, len(invalid_emails)):
         login_script(invalid_emails[i], None, registered_passwords[0])
-        assertCheck("Check invalid email test %d:" % (i+1), driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid e-mail.")
-
+        results["invalid"].append(assertCheck("Check invalid email test %d:" % (i+1), driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid e-mail."))
     for i  in range(0, len(unregistered_emails)):
         login_script(unregistered_emails[i], None, registered_passwords[0])
-        assertCheck("Check unregistered email test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute('innerHTML'), "There is no such account. Please try signing up.")
-
+        results["unregistered"].append(assertCheck("Check unregistered email test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute('innerHTML'), "There is no such account. Please try signing up."))
     for i  in range(0, len(registered_emails)):
         login_script(registered_emails[i], None, registered_passwords[i])
-        assertCheck("Check registered email test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute('innerHTML'), "Login Successful. Welcome " + registered_emails[i])
+        results["registered"].append(assertCheck("Check registered email test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute('innerHTML'), "Login Successful. Welcome " + registered_emails[i]))
+    return results
 
 def pass_check():
+    results = { "invalid": [], "unregistered": [], "registered": [] }
     for i in range(0, len(registered_emails)):
         login_script(registered_emails[i], None, invalid_passwords[i])
-        assertCheck("Invalid password test %d:" % (i+1), driver.find_element_by_id("passError").get_attribute("innerText"), "Password should be between 4 and 60 characters long.")
+        results["invalid"].append(assertCheck("Invalid password test %d:" % (i+1), driver.find_element_by_id("passError").get_attribute("innerText"), "Password should be between 4 and 60 characters long."))
         login_script(registered_emails[i], None, unregistered_passwords[i])
-        assertCheck("Unregistered password test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute("innerHTML"), "Wrong password. Please try again or reset your password.")
+        results["unregistered"].append(assertCheck("Unregistered password test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute("innerHTML"), "Wrong password. Please try again or reset your password."))
         login_script(registered_emails[i], None, registered_passwords[i])
-        assertCheck("Registered password test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute("innerHTML"), "Login Successful. Welcome " + registered_emails[i])
+        results["registered"].append(assertCheck("Registered password test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute("innerHTML"), "Login Successful. Welcome " + registered_emails[i]))
+    return results
 
 def phone_check():
+    results = { "invalid": [], "unregistered": [], "registered": [] }
     for i  in range(0, len(invalid_phnumbers)):
         login_script(None, invalid_phnumbers[i], registered_passwords[0])
-        assertCheck("Check invalid phone test %d:" % (i+1), driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid phone number.")
-
+        results["invalid"].append(assertCheck("Check invalid phone test %d:" % (i+1), driver.find_element_by_id("emailError").get_attribute('innerHTML'), "Please enter a valid phone number."))
     for i  in range(0, len(unregistered_phnumbers)):
         login_script(None, unregistered_phnumbers[i], registered_passwords[0])
-        assertCheck("Check unregistered phone test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute('innerHTML'), "There is no such account. Please try signing up.")
-
+        results["unregistered"].append(assertCheck("Check unregistered phone test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute('innerHTML'), "There is no such account. Please try signing up."))
     for i  in range(0, len(registered_phnumbers)):
         login_script(None, registered_phnumbers[i], registered_passwords[i])
-        assertCheck("Check registered phone test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute('innerHTML'), "Login Successful. Welcome " + registered_emails[i])
+        results["registered"].append(assertCheck("Check registered phone test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute('innerHTML'), "Login Successful. Welcome " + registered_emails[i]))
+    return results
 
+def compatibility_check():
+    allsuccess = True
+    if not res_spllc:
+        allsuccess = False
+        print("\t Signup-page sign in link test failed!")
+    if not res_lphbc:
+        allsuccess = False
+        print("\t Login-page home button test failed!")
+    if not res_lpsulc:
+        allsuccess = False
+        print("\t Login-page sign up link test failed!")
+    if not res_lphlc:
+        allsuccess = False
+        print("\t Login-page help link test failed!")
+    if not res_lppbc:
+        allsuccess = False
+        print("\t Login-page pass-hide button test failed!")
+    if not res_inc:
+        allsuccess = False
+        print("\t Invalid input test failed!")
+    for i in range(0, len(res_emc["invalid"])):
+        if not res_emc["invalid"][i]:
+            allsuccess = False
+            print("\t Invalid email test %d failed!" % (i + 1))
+    for i in range(0, len(res_emc["unregistered"])):
+        if not res_emc["unregistered"][i]:
+            allsuccess = False
+            print("\t Unregistered email test %d failed!" % (i + 1))
+    for i in range(0, len(res_emc["registered"])):
+        if not res_emc["registered"][i]:
+            allsuccess = False
+            print("\t Registered email test %d failed!" % (i + 1))
+    for i in range(0, len(res_phc["invalid"])):
+        if not res_phc["invalid"][i]:
+            allsuccess = False
+            print("\t Invalid phone test %d failed!" % (i + 1))
+    for i in range(0, len(res_phc["unregistered"])):
+        if not res_phc["unregistered"][i]:
+            allsuccess = False
+            print("\t Unregistered phone test %d failed!" % (i + 1))
+    for i in range(0, len(res_phc["registered"])):
+        if not res_phc["registered"][i]:
+            allsuccess = False
+            print("\t Registered phone test %d failed!" % (i + 1))
+    for i in range(0, len(res_pac["invalid"])):
+        if not res_pac["invalid"][i]:
+            allsuccess = False
+            print("\t Invalid password test %d failed!" % (i + 1))
+    for i in range(0, len(res_pac["unregistered"])):
+        if not res_pac["unregistered"][i]:
+            allsuccess = False
+            print("\t Unregistered password test %d failed!" % (i + 1))
+    for i in range(0, len(res_pac["registered"])):
+        if not res_pac["registered"][i]:
+            allsuccess = False
+            print("\t Registered password test %d failed!" % (i + 1))
+    if allsuccess:
+        print("\t All tests successfully passed!")
 
-driver = webdriver.Chrome() #Change if another browser or driver to use!
+if len(sys.argv) < 2:
+    print("Please specify a default driver: Chrome/Firefox/Opera")
+    sys.exit()
+
+if sys.argv[1] == "Chrome":
+    try:
+        driver = webdriver.Chrome()
+        driver_index = 0
+    except SessionNotCreatedException:
+        print("The specified browser executables not found! Please install or pick another driver")
+        sys.exit()
+    except WebDriverException:
+        print("The specified driver not found! Please download or pick another driver")
+        sys.exit()
+elif sys.argv[1] == "Firefox":
+    try:
+        driver = webdriver.Firefox()
+        driver_index = 1
+    except SessionNotCreatedException:
+        print("The specified browser executables not found! Please install or pick another driver")
+        sys.exit()
+    except WebDriverException:
+        print("The specified driver not found! Please download or pick another driver")
+        sys.exit()
+elif sys.argv[1] == "Opera":
+    try:
+        driver = webdriver.Opera()
+        driver_index = 2
+    except SessionNotCreatedException:
+        print("The specified browser executables not found! Please install or pick another driver")
+        sys.exit()
+    except WebDriverException:
+        print("The specified driver not found! Please download or pick another driver")
+        sys.exit()
+else:
+    print("Please specify the default driver correct! Chrome/Firefox/Opera")
+    sys.exit()
+
 url_path = "http://localhost:3000/"
 login_route = "login"
-signup_page_login_link_check()
-login_page_home_button_check()
-login_page_sign_up_link_check()
-login_page_help_link_check()
-login_page_passhide_button_check()
-input_check()
-email_check()
-phone_check()
-pass_check()
-driver.close()
+log_enabled = True
+skip = False
+for i in range(0, 3):
+    if not skip:
+        res_spllc = signup_page_login_link_check()
+        res_lphbc = login_page_home_button_check()
+        res_lpsulc = login_page_sign_up_link_check()
+        res_lphlc = login_page_help_link_check()
+        res_lppbc = login_page_passhide_button_check()
+        res_inc = input_check()
+        res_emc = email_check()
+        res_phc = phone_check()
+        res_pac = pass_check()
+        driver.close()
+        if driver_index == 0:
+            print("Chrome compatibility test:")
+            compatibility_check()
+        elif driver_index == 1:
+            print("Firefox compatibility test:")
+            compatibility_check()
+        else:
+            print("Opera compatibility test:")
+            compatibility_check()
+
+    driver_index +=1
+    if driver_index == 3:
+        driver_index = 0
+
+    skip = False
+    if not i == 2:
+        if driver_index == 0:
+            try:
+                driver = webdriver.Chrome()
+            except SessionNotCreatedException:
+                print("Chrome compatibility test:", "Test Failed. Chrome executables not found")
+                skip = True
+            except WebDriverException:
+                print("Chrome compatibility test:", "Test Failed. Chrome driver not found")
+                skip = True
+        elif driver_index == 1:
+            try:
+                driver = webdriver.Firefox()
+            except SessionNotCreatedException:
+                print("Firefox compatibility test:", "Test Failed. Firefox executables not found")
+                skip = True
+            except WebDriverException:
+                print("Firefox compatibility test:", "Test Failed. Firefox driver not found")
+                skip = True
+        else:
+            try:
+                driver = webdriver.Opera()
+            except SessionNotCreatedException:
+                print("Opera compatibility test:", "Test Failed. Opera executables not found")
+                skip = True
+            except WebDriverException:
+                print("Opera compatibility test:", "Test Failed. Opera driver not found")
+                skip = True
+        log_enabled = False
+
+
 
 
