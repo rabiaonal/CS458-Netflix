483cc215d74a1642786ebbcc950835bdecc13a1b
burak mutlu <burak_mutlu_97@hotmail.com>
2021-02-19 19:00:29 +0300

    test log changes

--- src/Test.py
+++ src/Test.py
@@ -1,7 +1,7 @@
 from time import sleep
 from selenium import webdriver
 
-invalid_emails = ["emai", "@ema", "mail", "asdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfgasdfga"] #Email must be between 5 and 50 characters
+invalid_emails = ["emai", "@ema", "mail", "toolongcharacteremailabout50charactersapplicationshouldgiveerror"] #Email must be between 5 and 50 characters
 unregistered_emails = ["invaliduser1@gmail.com", "invaliduser2@gmail.com", "invaliduser3@gmail.com"]
 registered_emails = ["kenneth@gmail.com", "john@gmail.com", "mary@gmail.com"]
 
@@ -16,24 +16,24 @@ registered_passwords = ["329129293", "1234", "123123123"] #Should be sorted with
 def assertCheck(test_descriptor, input, expected):
     try:
         assert input == expected
-        print(test_descriptor, "Test Successful.", "Expected Result: ", expected)
+        print(test_descriptor, "Test Successful.", "Expected Result:", expected)
     except AssertionError:
-        print(test_descriptor, "Test Failed.", "Expected Result: ", expected)
+        print(test_descriptor, "Test Failed.", "Expected Result:", expected, "Obtained Result:", input)
 
 def index_page_sign_in_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signInBtn").click()
-    assertCheck("Index page sign in button test", driver.current_url, "http://localhost:3000/login")
+    assertCheck("Index-page sign in button test:", driver.current_url, "http://localhost:3000/login")
 
 def index_page_sign_up_button_check():
     driver.get("http://localhost:3000/")
     driver.find_element_by_id("signUpBtn").click()
-    assertCheck("Index page sign up button test", driver.current_url, "http://localhost:3000/signup")
+    assertCheck("Index-page sign up button test:", driver.current_url, "http://localhost:3000/signup")
 
 def login_page_home_button_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("homebutton").click()
-    assertCheck("Login page home button test", driver.current_url, "http://localhost:3000/")
+    assertCheck("Login-page home button test:", driver.current_url, "http://localhost:3000/")
 
 def login_page_passhide_button_check():
     driver.get("http://localhost:3000/login")
@@ -46,24 +46,24 @@ def login_page_passhide_button_check():
     input_type_3 = input.get_attribute("type")
     try:
         assert (input_type_1 != input_type_2) & (input_type_1 == input_type_3)
-        print("Login page pass/hide button test: ", "Test Succesful.")
+        print("Login-page pass-hide button test:", "Test Successful.")
     except AssertionError:
-        print("Login page pass/hide button test: ", "Test Failed.")
+        print("Login-page pass-hide button test:", "Test Failed.")
 
 def login_page_sign_up_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("signupLink").click()
-    assertCheck("Login page sign up link test", driver.current_url, "http://localhost:3000/signup")
+    assertCheck("Login-page sign up link test:", driver.current_url, "http://localhost:3000/signup")
 
 def login_page_help_link_check():
     driver.get("http://localhost:3000/login")
     driver.find_element_by_id("forgotpasswordLink").click()
-    assertCheck("Login page help up link test", driver.current_url, "http://localhost:3000/resetpass")
+    assertCheck("Login-page help link test:", driver.current_url, "http://localhost:3000/resetpass")
 
 def signup_page_login_link_check():
     driver.get("http://localhost:3000/signup")
     driver.find_element_by_id("loginLink").click()
-    assertCheck("Sign up page login link test", driver.current_url, "http://localhost:3000/login")
+    assertCheck("Signup-page sign in link test:", driver.current_url, "http://localhost:3000/login")
 
 def login_script(email, password):
     print("Login with email: %s, password: %s" % (email, password))
@@ -73,20 +73,20 @@ def login_script(email, password):
     driver.find_element_by_id("loginSubmit").click()
 
 def email_check():
-    for email in invalid_emails:
-        login_script(email, registered_passwords[0])
+    for i  in range(0, len(invalid_emails)):
+        login_script(invalid_emails[i], registered_passwords[0])
         email_error = driver.find_element_by_id("emailError")
-        assertCheck("Check invalid email", email_error.get_attribute('innerHTML'), "Please enter a valid e-mail.")
+        assertCheck("Check invalid email test %d:" % (i+1), email_error.get_attribute('innerHTML'), "Please enter a valid e-mail.")
 
-    for email in unregistered_emails:
-        login_script(email, registered_passwords[0])
+    for i  in range(0, len(unregistered_emails)):
+        login_script(unregistered_emails[i], unregistered_passwords[0])
         login_error = driver.find_element_by_id("loginError")
-        assertCheck("Check unregistered email", login_error.get_attribute('innerHTML'), "There is no such account. Please try signing up.")
+        assertCheck("Check unregistered email test %d:" % (i+1), login_error.get_attribute('innerHTML'), "There is no such account. Please try signing up.")
 
-    for i, email in enumerate(registered_emails, start=0):
-        login_script(email, registered_passwords[i])
+    for i  in range(0, len(registered_emails)):
+        login_script(registered_emails[i], registered_passwords[i])
         welcome = driver.find_element_by_tag_name("h1")
-        assertCheck("Check registered email", welcome.get_attribute('innerHTML'), "Login Successful. Welcome " + email)
+        assertCheck("Check registered email test %d:" % (i+1), welcome.get_attribute('innerHTML'), "Login Successful. Welcome " + registered_emails[i])
 
 def pass_check():
     for i in range(0, len(registered_emails)):
@@ -95,7 +95,7 @@ def pass_check():
         login_script(registered_emails[i], unregistered_passwords[i])
         assertCheck("Unregistered password test %d:" % (i+1), driver.find_element_by_id("loginError").get_attribute("innerHTML"), "Wrong password. Please try again or reset your password.")
         login_script(registered_emails[i], registered_passwords[i])
-        assertCheck("Registered password test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute("innerHTML"), "Login Successful. Welcome %s" % (registered_emails[i]))
+        assertCheck("Registered password test %d:" % (i+1), driver.find_element_by_tag_name("h1").get_attribute("innerHTML"), "Login Successful. Welcome " + registered_emails[i])
 
 def phone_check():
     pass
